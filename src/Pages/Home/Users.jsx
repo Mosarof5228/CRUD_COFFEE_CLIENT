6b9ee8d3f92b6e77f7fcab6e44d967aa671b7244
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2024-09-30 23:17:13 +0600

    delete api with fetch done

--- src/Pages/Home/Users.jsx
+++ src/Pages/Home/Users.jsx
@@ -1,10 +1,51 @@
+import Swal from "sweetalert2";
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
     const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
+    const handleDelete = (_id) => {
+        console.log("Delete is coming", _id)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/users/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                        const remaingUsers = users.filter(user => user._id !== _id);
+                        setUsers(remaingUsers);
+
+
+                    })
+
+
+
+
+            }
+        });
+
+    }
     return (
         <div>
-            <h2>Here its showing all users: {loadedUsers.length}</h2>
+            <h2>Here its showing all users: {users.length}</h2>
             <div>
                 <div className="overflow-x-auto">
                     <table className="table">
@@ -21,12 +62,12 @@ const Users = () => {
                         <tbody>
                             {/* row 1 */}
                             {
-                                loadedUsers.map((user, index) =>
+                                users.map((user, index) =>
                                     <tr className="" key={user._id}>
                                         <th>{index + 1}</th>
                                         <td>{user.email}</td>
                                         <td>{user?.createdTime}</td>
-                                        <td className="cursor-pointer">Delete</td>
+                                        <td onClick={() => handleDelete(user._id)} className="cursor-pointer">Delete</td>
                                         <td className="cursor-pointer">Update</td>
                                     </tr>
                                 )
